7df7f005e2ed1c489024b136e639754fd70aff48
loser8p <loser8p@gmail.com>
2024-08-08 21:29:07 +0800

    test

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-# import google_sheets
+import  google_sheet
 import os
 
 app = Flask(__name__)
@@ -12,6 +12,7 @@ CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+
 
 @app.route("/api/callback", methods=['POST'])
 def callback():
@@ -33,7 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text.split(',')
-    # google_sheets.append_row(text)
+    google_sheet.append_row(text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='数据已保存至Google Sheets!')
